2566bb61f93c39e7d0e89eee09b25e27304f95fb
Arnaud Castelltort <arnaud.castelltort@gmail.com>
2011-04-13 00:19:51 +0200

    [Hack] add event job for 1st jug event

--- app/notifiers/Mails.java
+++ app/notifiers/Mails.java
@@ -1,19 +1,15 @@
 package notifiers;
 
-import org.apache.commons.mail.EmailAttachment;
-
 import models.Event;
-import play.Play;
-import play.mvc.Http.Request;
 import play.mvc.Mailer;
 
 public class Mails extends Mailer {
 
 	static public void preRegistrationMail(Event event, String email, String host, String code)
 	{
-		setSubject("JUG-Montpellier: "+event.title + ": PreRegistration");
-		addRecipient(email);
-		setFrom("info@jug-montpellier.org");
+        setSubject("JUG-Montpellier: " + event.title + ": PreRegistration");
+        addRecipient(email);
+        setFrom("info@jug-montpellier.org");
 //		EmailAttachment cssAttachment = new EmailAttachment();
 //	      cssAttachment.setDescription("CSS File");
 //	      cssAttachment.setPath(Play.getFile("public/stylesheets/main.css").getPath());
@@ -26,5 +22,13 @@ public class Mails extends Mailer {
 //	      addAttachment(logoAttachment);
 		send(event,host, code);
 	}
+
+    public static void confirmationMail(Event event, String email, String host, String code) {
+        setSubject("JUG-Montpellier: " + event.title + ": Confirmation");
+        addRecipient(email);
+        setFrom("info@jug-montpellier.org");
+
+        send(event,host, code);
+    }
 
 }
